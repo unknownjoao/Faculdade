9f159b4b9dcd10ed71339857b6752a129e9ab505
João Lucas <113278072+unknownjoao@users.noreply.github.com>
2022-09-27 00:28:42 -0400

    Update App.java

--- MatrizesBidimensionalAtividade/src/App.java
+++ MatrizesBidimensionalAtividade/src/App.java
@@ -2,17 +2,6 @@ import java.util.Scanner;
 
 public class App 
 {
-
-    /*
-     * Faça um programa obrigatoriamente em JAVA que:
-     * 1. Leia o tamanho de uma matriz bidimensional
-     * 2. Crie uma matriz bidimensional com as dimensões informadas
-     * 3. Preencha cada elemento da matriz com um valor aleatório de 0 a 9
-     * 4. Mostre a matriz preenchida na tela
-     * 5. Some todos os elementos de cada linha da matriz
-     * 6. Mostrar a linha da matriz que tiver a maior soma.
-     */
-
     public static void main(String[] args) throws Exception
     {
 
